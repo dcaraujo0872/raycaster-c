6c1920707f63d92c7e88bae8eee41e5ffc2b116f
Christopher Araujo <christopher.araujo@hey.com>
2020-08-26 10:23:51 +0200

    Code cleanup

--- src/main.c
+++ src/main.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-#include <limits.h>
 #include <SDL2/SDL.h>
-#include "upng.h"
-#include "constants.h"
 #include "ray.h"
 #include "player.h"
 #include "map.h"
-#include "utils.h"
+#include "upng.h"
+#include "constants.h"
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
@@ -20,176 +17,6 @@ upng_t* pngTexture;
 Player player;
 Ray rays[NUM_RAYS];
 
-void castRay(Ray *ray) {
-    float xintercept, yintercept;
-    float xstep, ystep;
-
-    ///////////////////////////////////////////
-    // HORIZONTAL RAY-GRID INTERSECTION CODE
-    ///////////////////////////////////////////
-    int foundHorzWallHit = false;
-    float horzWallHitX = 0;
-    float horzWallHitY = 0;
-    int horzWallContent = 0;
-
-    // Find the y-coordinate of the closest horizontal grid intersection
-    yintercept = floor(player.y / TILE_SIZE) * TILE_SIZE;
-    yintercept += isRayFacingDown(ray->rayAngle) ? TILE_SIZE : 0;
-
-    // Find the x-coordinate of the closest horizontal grid intersection
-    xintercept = player.x + (yintercept - player.y) / tan(ray->rayAngle);
-
-    // Calculate the increment xstep and ystep
-    ystep = TILE_SIZE;
-    ystep *= isRayFacingUp(ray->rayAngle) ? -1 : 1;
-
-    xstep = TILE_SIZE / tan(ray->rayAngle);
-    xstep *= (isRayFacingLeft(ray->rayAngle) && xstep > 0) ? -1 : 1;
-    xstep *= (isRayFacingRight(ray->rayAngle) && xstep < 0) ? -1 : 1;
-
-    float nextHorzTouchX = xintercept;
-    float nextHorzTouchY = yintercept;
-
-    // Increment xstep and ystep until we find a wall
-    while (nextHorzTouchX >= 0 && nextHorzTouchX <= WINDOW_WIDTH && nextHorzTouchY >= 0 && nextHorzTouchY <= WINDOW_HEIGHT) {
-        float xToCheck = nextHorzTouchX;
-        float yToCheck = nextHorzTouchY + (isRayFacingUp(ray->rayAngle) ? -1 : 0);
-
-        if (mapHasWallAt(xToCheck, yToCheck)) {
-            // found a wall hit
-            horzWallHitX = nextHorzTouchX;
-            horzWallHitY = nextHorzTouchY;
-            horzWallContent = map[(int)floor(yToCheck / TILE_SIZE)][(int)floor(xToCheck / TILE_SIZE)];
-            foundHorzWallHit = true;
-            break;
-        } else {
-            nextHorzTouchX += xstep;
-            nextHorzTouchY += ystep;
-        }
-    }
-
-    ///////////////////////////////////////////
-    // VERTICAL RAY-GRID INTERSECTION CODE
-    ///////////////////////////////////////////
-    int foundVertWallHit = false;
-    float vertWallHitX = 0;
-    float vertWallHitY = 0;
-    int vertWallContent = 0;
-
-    // Find the x-coordinate of the closest horizontal grid intersection
-    xintercept = floor(player.x / TILE_SIZE) * TILE_SIZE;
-    xintercept += isRayFacingRight(ray->rayAngle) ? TILE_SIZE : 0;
-
-    // Find the y-coordinate of the closest horizontal grid intersection
-    yintercept = player.y + (xintercept - player.x) * tan(ray->rayAngle);
-
-    // Calculate the increment xstep and ystep
-    xstep = TILE_SIZE;
-    xstep *= isRayFacingLeft(ray->rayAngle) ? -1 : 1;
-
-    ystep = TILE_SIZE * tan(ray->rayAngle);
-    ystep *= (isRayFacingUp(ray->rayAngle) && ystep > 0) ? -1 : 1;
-    ystep *= (isRayFacingDown(ray->rayAngle) && ystep < 0) ? -1 : 1;
-
-    float nextVertTouchX = xintercept;
-    float nextVertTouchY = yintercept;
-
-    // Increment xstep and ystep until we find a wall
-    while (nextVertTouchX >= 0 && nextVertTouchX <= WINDOW_WIDTH && nextVertTouchY >= 0 && nextVertTouchY <= WINDOW_HEIGHT) {
-        float xToCheck = nextVertTouchX + (isRayFacingLeft(ray->rayAngle) ? -1 : 0);
-        float yToCheck = nextVertTouchY;
-
-        if (mapHasWallAt(xToCheck, yToCheck)) {
-            // found a wall hit
-            vertWallHitX = nextVertTouchX;
-            vertWallHitY = nextVertTouchY;
-            vertWallContent = map[(int)floor(yToCheck / TILE_SIZE)][(int)floor(xToCheck / TILE_SIZE)];
-            foundVertWallHit = true;
-            break;
-        } else {
-            nextVertTouchX += xstep;
-            nextVertTouchY += ystep;
-        }
-    }
-
-    // Calculate both horizontal and vertical hit distances and choose the smallest one
-    float horzHitDistance = foundHorzWallHit
-        ? distanceBetweenPoints(player.x, player.y, horzWallHitX, horzWallHitY)
-        : INT_MAX;
-    float vertHitDistance = foundVertWallHit
-        ? distanceBetweenPoints(player.x, player.y, vertWallHitX, vertWallHitY)
-        : INT_MAX;
-
-    if (vertHitDistance < horzHitDistance) {
-        ray->distance = vertHitDistance;
-        ray->wallHitX = vertWallHitX;
-        ray->wallHitY = vertWallHitY;
-        ray->wallHitContent = vertWallContent;
-        ray->wasHitVertical = true;
-    } else {
-        ray->distance = horzHitDistance;
-        ray->wallHitX = horzWallHitX;
-        ray->wallHitY = horzWallHitY;
-        ray->wallHitContent = horzWallContent;
-        ray->wasHitVertical = false;
-    }
-}
-
-void castAllRays(void) {
-    float rayAngle = player.rotationAngle - (FOV_ANGLE / 2);
-    for (int i = 0; i < NUM_RAYS; i++) {
-        rays[i].rayAngle = normalizeAngle(rayAngle);
-        castRay(&rays[i]);
-        rayAngle += FOV_ANGLE / NUM_RAYS;
-    }
-}
-
-void generate3DProjection(void) {
-    for (int i = 0; i < NUM_RAYS; i++) {
-        float perpDistance = rays[i].distance * cos(rays[i].rayAngle - player.rotationAngle);
-        float distanceProjPlane = (WINDOW_WIDTH / 2) / tan(FOV_ANGLE / 2);
-        float projectedWallHeight = (TILE_SIZE / perpDistance) * distanceProjPlane;
-
-        int wallStripHeight = (int)projectedWallHeight;
-
-        int wallTopPixel = (WINDOW_HEIGHT / 2) - (wallStripHeight / 2);
-        wallTopPixel = wallTopPixel < 0 ? 0 : wallTopPixel;
-
-        int wallBottomPixel = (WINDOW_HEIGHT / 2) + (wallStripHeight / 2);
-        wallBottomPixel = wallBottomPixel > WINDOW_HEIGHT ? WINDOW_HEIGHT : wallBottomPixel;
-
-        // set the color of the ceiling
-        for (int y = 0; y < wallTopPixel; y++)
-            colorBuffer[(WINDOW_WIDTH * y) + i] = 0xFF444444;
-
-        int textureOffsetX;
-        if (rays[i].wasHitVertical) {
-            textureOffsetX = (int)rays[i].wallHitY % TILE_SIZE;
-        }
-        else {
-            textureOffsetX = (int)rays[i].wallHitX % TILE_SIZE;
-        }
-
-        // get the correct texture id number from the map content
-        int texNum = rays[i].wallHitContent - 1;
-
-        // render the wall from wallTopPixel to wallBottomPixel
-        for (int y = wallTopPixel; y < wallBottomPixel; y++) {
-            int distanceFromTop = y + (wallStripHeight / 2) - (WINDOW_HEIGHT / 2);
-            int textureOffsetY = distanceFromTop * ((float)TEXTURE_HEIGHT / wallStripHeight);
-
-            // set the color of the wall texture based on the color from the texture in memory
-            uint32_t texelColor = wallTexture[(TEXTURE_WIDTH * textureOffsetY) + textureOffsetX];
-            colorBuffer[(WINDOW_WIDTH * y) + i] = texelColor;
-        }
-
-        // set the color of the floor
-        for (int y = wallBottomPixel; y < WINDOW_HEIGHT; y++) {
-            colorBuffer[(WINDOW_WIDTH * y) + i] = 0xFF888888;
-        }
-    }
-}
-
 int initializeWindow(void);
 void setup(void);
 void processInput(void);
@@ -198,6 +25,7 @@ void render(void);
 void destroyWindow(void);
 void clearColorBuffer(uint32_t color);
 void renderColorBuffer(void);
+void generate3DProjection(void);
 
 int main() {
     isGameRunning = initializeWindow();
@@ -329,7 +157,7 @@ void update(void) {
     ticksLastFrame = SDL_GetTicks();
 
     movePlayer(&player, deltaTime);
-    castAllRays();
+    castAllRays(rays, &player);
 }
 
 void render(void) {
@@ -370,4 +198,50 @@ void renderColorBuffer(void) {
         (int)((uint32_t)WINDOW_WIDTH * sizeof(uint32_t))
     );
     SDL_RenderCopy(renderer, colorBufferTexture, NULL, NULL);
+}
+
+void generate3DProjection(void) {
+    for (int i = 0; i < NUM_RAYS; i++) {
+        float perpDistance = rays[i].distance * cos(rays[i].angle - player.rotationAngle);
+        float distanceProjPlane = (WINDOW_WIDTH / 2) / tan(FOV_ANGLE / 2);
+        float projectedWallHeight = (TILE_SIZE / perpDistance) * distanceProjPlane;
+
+        int wallStripHeight = (int)projectedWallHeight;
+
+        int wallTopPixel = (WINDOW_HEIGHT / 2) - (wallStripHeight / 2);
+        wallTopPixel = wallTopPixel < 0 ? 0 : wallTopPixel;
+
+        int wallBottomPixel = (WINDOW_HEIGHT / 2) + (wallStripHeight / 2);
+        wallBottomPixel = wallBottomPixel > WINDOW_HEIGHT ? WINDOW_HEIGHT : wallBottomPixel;
+
+        // set the color of the ceiling
+        for (int y = 0; y < wallTopPixel; y++)
+            colorBuffer[(WINDOW_WIDTH * y) + i] = 0xFF444444;
+
+        int textureOffsetX;
+        if (rays[i].wasHitVertical) {
+            textureOffsetX = (int)rays[i].wallHitY % TILE_SIZE;
+        }
+        else {
+            textureOffsetX = (int)rays[i].wallHitX % TILE_SIZE;
+        }
+
+        // get the correct texture id number from the map content
+        int texNum = rays[i].wallHitContent - 1;
+
+        // render the wall from wallTopPixel to wallBottomPixel
+        for (int y = wallTopPixel; y < wallBottomPixel; y++) {
+            int distanceFromTop = y + (wallStripHeight / 2) - (WINDOW_HEIGHT / 2);
+            int textureOffsetY = distanceFromTop * ((float)TEXTURE_HEIGHT / wallStripHeight);
+
+            // set the color of the wall texture based on the color from the texture in memory
+            uint32_t texelColor = wallTexture[(TEXTURE_WIDTH * textureOffsetY) + textureOffsetX];
+            colorBuffer[(WINDOW_WIDTH * y) + i] = texelColor;
+        }
+
+        // set the color of the floor
+        for (int y = wallBottomPixel; y < WINDOW_HEIGHT; y++) {
+            colorBuffer[(WINDOW_WIDTH * y) + i] = 0xFF888888;
+        }
+    }
 }
